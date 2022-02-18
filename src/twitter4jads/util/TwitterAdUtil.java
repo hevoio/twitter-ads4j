c1abeb13634416c40634bd994087f0602a6860c3
Adithya Aravind <macpro@Adithyas-MacBook-Pro.local>
2022-02-18 11:28:54 +0530

    Changed files in util

--- src/twitter4jads/util/TwitterAdUtil.java
+++ src/twitter4jads/util/TwitterAdUtil.java
@@ -27,17 +27,6 @@ import static twitter4jads.models.ads.TwitterAdObjective.WEBSITE_CONVERSIONS;
  */
 public final class TwitterAdUtil {
 
-    public static final String FOLLOWERS = "FOLLOWERS";
-    public static final String ENGAGEMENTS = "ENGAGEMENTS";
-    public static final String VIDEO_VIEWS = "VIDEO_VIEWS";
-    public static final String WEBSITE_CLICKS = "WEBSITE_CLICKS";
-    public static final String VIDEO_VIEWS_PREROLL = "VIDEO_VIEWS_PREROLL";
-    public static final String APP_ENGAGEMENTS = "APP_ENGAGEMENTS";
-    public static final String APP_INSTALLS = "APP_INSTALLS";
-    public static final Set<TwitterAdObjective> TAP_SUPPORTED_OBJECTIVES =
-        unmodifiableSet(newHashSet(APP_INSTALLS, APP_ENGAGEMENTS, PREROLL_VIEWS, WEBSITE_CLICKS, WEBSITE_CONVERSIONS));
-
-    public static final Set<TwitterAdObjective> TARGET_CPA_SUPPORTED_OBJECTIVES = unmodifiableSet(newHashSet(WEBSITE_CONVERSIONS));
     public static final String UTC_TMZ = "UTC";
     public static final String FORMAT_YYYYMMDD_HHMM = "yyyyMMdd_HHmm";
 
@@ -47,23 +36,6 @@ public final class TwitterAdUtil {
             return Calendar.getInstance(TimeZone.getTimeZone(UTC_TMZ));
         }
     };
-
-    public static final ThreadLocal<SimpleDateFormat> FORMATTER_UTC_YYYYMMDD_HHMM = new ThreadLocal<SimpleDateFormat>() {
-        @Override
-        protected SimpleDateFormat initialValue() {
-            SimpleDateFormat rv = new SimpleDateFormat(FORMAT_YYYYMMDD_HHMM);
-            rv.setCalendar(UTC_CALENDAR.get());
-            return rv;
-        }
-    };
-
-    public static String convertTimeToZuluFormatAndToUTC(long time) {
-        String dateFormat = "yyyy-MM-dd'T'HH:mm:ss'Z'";
-
-        DateTime dateTimeStart = new DateTime(time, DateTimeZone.UTC);
-
-        return dateTimeStart.toString(dateFormat);
-    }
 
     public static <T> String getCsv(Collection<T> collection) {
         String result = "";
@@ -94,30 +66,14 @@ public final class TwitterAdUtil {
         return object != null;
     }
 
-    public static <T> void containedInList(T object, List<T> list, String message) {
-        if (list == null || !list.contains(object)) {
-            throw new IllegalArgumentException(message);
-        }
-    }
-
     public static void ensureNotNull(Object object, String name) {
         if (object == null) {
             throw new IllegalArgumentException(name + " can not be null.");
         }
     }
 
-    public static <T> void ensureNotEmpty(Collection<T> collection, String name) {
-        if (!isNotEmpty(collection)) {
-            throw new IllegalArgumentException(name + " can not be null or empty.");
-        }
-    }
-
     public static <T> boolean isNotEmpty(Collection<T> collection) {
         return collection != null && collection.size() != 0;
-    }
-
-    public static <T> boolean isEmpty(Collection<T> collection) {
-        return collection == null || collection.size() == 0;
     }
 
     public static <T> Boolean ensureMaxSize(Collection<T> collection, int size) {
@@ -139,18 +95,6 @@ public final class TwitterAdUtil {
         return mutableList;
     }
 
-    public static String getDelimiterSeparatedMethod(final Collection<String> values, String delimiter) {
-        if (values == null || values.isEmpty()) {
-            return "";
-        }
-        String rv = "";
-        for (String value : values) {
-            rv = rv + value + delimiter;
-        }
-        rv = rv.substring(0, rv.length() - 1);
-        return rv;
-    }
-
     public static <T> BaseAdsResponse<T> constructBaseAdsResponse(HttpResponse httpResponse, String response, Type type) throws IOException {
         if (type == null) {
             return null;
@@ -160,41 +104,6 @@ public final class TwitterAdUtil {
         RateLimitStatus rateLimitStatus = TwitterAdHttpUtils.createFromResponseHeader(httpResponse);
         baseResponse.setRateLimitStatus(rateLimitStatus);
         return baseResponse;
-    }
-
-    public static <T> BaseAdsListResponse<T> constructBaseAdsListResponse(HttpResponse httpResponse, String response, Type type) throws IOException {
-        Gson gson = new Gson();
-        BaseAdsListResponse<T> baseResponse = gson.fromJson(response, type);
-        if (baseResponse == null) {
-            return null;
-        }
-        RateLimitStatus rateLimitStatus = TwitterAdHttpUtils.createFromResponseHeader(httpResponse);
-        baseResponse.setRateLimitStatus(rateLimitStatus);
-        return baseResponse;
-    }
-
-    public static void reallySleep(long millis) {
-        boolean threadInterrupted = false;
-        final long nanos = TimeUnit.MILLISECONDS.toNanos(millis);
-        final long end = System.nanoTime() + nanos;
-        long remaining;
-        try {
-            do {
-                remaining = end - System.nanoTime();
-                if (remaining <= 0) {
-                    break;
-                }
-                try {
-                    Thread.sleep(TimeUnit.NANOSECONDS.toMillis(remaining));
-                } catch (InterruptedException e) {
-                    threadInterrupted = true;
-                }
-            } while (remaining > 0);
-        } finally {
-            if (threadInterrupted) {
-                Thread.currentThread().interrupt();
-            }
-        }
     }
 
 }
